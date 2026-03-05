c4bfda9cc755a38bcabb25513a34c30926d12b04
Poll0L0ko <al25120417@utcj.edu.mx>
2026-03-04 19:32:56 -0700

    Fix test

--- tests/test_facrtorial.py
+++ tests/test_facrtorial.py
@@ -2,7 +2,7 @@ import pytest
 from factorial import factorial
 
 def test_factorial_caso_correcto():
-    assert factorial(5) == 121
+    assert factorial(5) == 120
 
 def test_factorial_caso_limite():
     assert factorial(0) == 1
